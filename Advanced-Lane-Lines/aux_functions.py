271ce0df58ca2a95eca3dfd90b54d3fefc1f0e06
rui sp <manuelsapereira@gmail.com>
2018-12-05 18:05:47 +0800

    ready for applying the pipeline to video.

--- Advanced-Lane-Lines/aux_functions.py
+++ Advanced-Lane-Lines/aux_functions.py
@@ -7,8 +7,8 @@ Created on Fri Nov 30 11:23:04 2018
 
 import numpy as np
 import cv2
-import numpy.polynomial.polynomial as poly
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 def pipeline_grad_color_thresh(img, s_thresh=(120, 255), sx_thresh=(20, 140)):
     # Convert to HLS color space and separate the S channel
@@ -57,7 +57,7 @@ def corners_unwarp(img, nx, ny, mtx, dist):
        
     return warped, M
 
-# The function I used in the test quizes
+
 def find_lane_pixels(binary_warped):
     # Take a histogram of the bottom half of the image
     histogram = np.sum(binary_warped[binary_warped.shape[0]//2:,:], axis=0)
@@ -69,19 +69,15 @@ def find_lane_pixels(binary_warped):
     leftx_base = np.argmax(histogram[:midpoint])
     rightx_base = np.argmax(histogram[midpoint:]) + midpoint
 
-    # HYPERPARAMETERS
-    # Choose the number of sliding windows
-    nwindows = 9
-
-
+    # Set the number of windows
+    nwindows=9
     # Set the width of the windows +/- margin
-    margin = 100
+    margin = 50
     # Set minimum number of pixels found to recenter window
-    minpix = 20
+    minpix = 100
 
     # Set height of windows - based on nwindows above and image shape
     window_height = np.int(binary_warped.shape[0]//nwindows)
-    print(window_height)
     # Identify the x and y positions of all nonzero pixels in the image
     nonzero = binary_warped.nonzero()
     #print(binary_warped.shape)
@@ -108,11 +104,11 @@ def find_lane_pixels(binary_warped):
         win_xright_low =rightx_current-margin
         win_xright_high = rightx_current+margin # Update this
         
-        # Draw the windows on the visualization image
-        cv2.rectangle(out_img,(win_xleft_low,win_y_low),
-        (win_xleft_high,win_y_high),(0,255,0), 2) 
-        cv2.rectangle(out_img,(win_xright_low,win_y_low),
-        (win_xright_high,win_y_high),(0,255,0), 2) 
+        # We do not draw the windows on the visualization image, but keep the code for reference
+        #cv2.rectangle(out_img,(win_xleft_low,win_y_low),
+        #(win_xleft_high,win_y_high),(255,255,0), 2) 
+        #cv2.rectangle(out_img,(win_xright_low,win_y_low),
+        # (win_xright_high,win_y_high),(255,255,0), 2) 
         
         ###Identify the nonzero pixels in x and y within the window ###
         good_left_inds = binary_warped[win_y_low:win_y_high,win_xleft_low:win_xleft_high].nonzero()
@@ -131,15 +127,8 @@ def find_lane_pixels(binary_warped):
             leftx_current=int(good_left_inds[1].mean())
         if len(good_right_inds[0]) > minpix:
             rightx_current=int(good_right_inds[1].mean())
-    
-    print("........................")
-    
-
     left_lane_inds=np.concatenate(left_lane_inds,axis=1)
     right_lane_inds = np.concatenate(right_lane_inds,axis=1)
-  
-
-   
     leftx = left_lane_inds[1]
     lefty=left_lane_inds[0]
    
@@ -181,7 +170,36 @@ def fit_polynomial(binary_warped):
     plt.plot(left_fitx,ploty, color='yellow')
     plt.plot(right_fitx, ploty, color='yellow')
 
-    return out_img
+    return left_fit, right_fit, out_img
+
+
+def measure_curvature_distance_real(binary_warped,left_fit, right_fit,x_l,y_l):
+    '''
+    Calculates the curvature  of the road and the vehicle distance from the center in meters.
+    '''
+    # Define conversions in x and y from pixels space to meters using the measures defined in the notebook.
+    ym_per_pix = 30/y_l # meters per pixel in y dimension
+    xm_per_pix = 3.7/x_l # meters per pixel in x dimension
+
+    # Define y-value where we want radius of curvature
+    # We'll choose the maximum y-value, corresponding to the bottom of the image
+    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+    y_eval = np.max(ploty)*ym_per_pix
+    
+    left_curverad = (1+(2*left_fit[0]*y_eval+left_fit[1])**2)**(3/2)/(2*np.abs(left_fit[0]))
+    
+    right_curverad = (1+(2*right_fit[0]*y_eval+right_fit[1])**2)**(3/2)/(2*np.abs(right_fit[0])) 
+    
+    # Determine the center of the image
+    center_x=binary_warped.shape[1]//2
+    center_y=binary_warped.shape[0]//2
+    # The height of the points is the same, the center of the lane is at the level of the center of the image, and is given as the midpoint between the left and right polynomial fit given at y=binary_warped.shape[0]//2
+    left_midx = left_fit[0]*center_y**2 + left_fit[1]*center_y + left_fit[2]
+    right_midx = right_fit[0]*center_y**2 + right_fit[1]*center_y + right_fit[2]
+    mid_lane_x=(left_midx+right_midx)//2
+    cv2.line(binary_warped,(np.int(center_x),np.int(center_y)),(np.int(mid_lane_x),np.int(center_y)),(255,255,0),2)
+    distance=np.abs(mid_lane_x - midpoint_x) * xm_per_pix
+    return left_curverad, right_curverad, distance, binary_warped
 
 
 
